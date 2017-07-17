80040580aaf36e1d1ed5476d8e05129f624cc410
bukhtawar <bukhtawarelahi@gmail.com>
2017-07-17 23:01:22 +0500

    made changes in import files .py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
-import urllib
+ifrom __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
-from flask import Flask,request
+from flask import Flask
 from flask import request
 from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
+
 
 
 @app.route('/webhook', methods=['POST'])
@@ -51,8 +59,8 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-app.run()
-    #port = int(os.getenv('PORT', 5000))
+#app.run()
+    port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
 
